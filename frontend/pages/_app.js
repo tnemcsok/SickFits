855286749575a62a0e848da1908a22bacf6a8d2f
Tamas Nemcsok <tamas.nemcsok@gmail.com>
2021-06-22 18:08:47 +0200

    First Commit

---
+++ frontend/pages/_app.js
@@ -0,0 +1,36 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
+import { ApolloProvider } from '@apollo/client';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import Page from '../components/Page';
+import '../public/static/nprogress.css';
+import withData from '../lib/withData';
+import { CartStateProvider } from '../lib/cartState';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
+function MyApp({ Component, pageProps, apollo }) {
+  return (
+    <ApolloProvider client={apollo}>
+      <CartStateProvider>
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </CartStateProvider>
+    </ApolloProvider>
+  );
+}
+
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(MyApp);
---
+++ readme.md
@@ -0,0 +1,5 @@
+This application is an online store called Sick Fits.
+
+It's GraphQL API with Node.js on the backend and React and Apollo on the front end.
+
+It covers everything from authentication and sending email to uploading photos and caching data. The entire front-end is built in modern React.js and GraphQL.
